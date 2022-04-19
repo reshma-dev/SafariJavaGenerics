85a99f5fa80e2515fe96c1abae0e630262b62372
reshma-dev <reshma.sahasrabudhe@gmail.com>
2022-04-19 10:53:14 +0100

    update hello world message

--- src/main/java/verify/Verify.java
+++ src/main/java/verify/Verify.java
@@ -5,6 +5,6 @@ import java.util.function.Consumer;
 public class Verify {
   public static void main(String[] args) {
     ((Consumer<String>)(var s) -> System.out.println(s))
-        .accept("Hello World!");
+        .accept("Hello World! yet again...");
   }
 }
